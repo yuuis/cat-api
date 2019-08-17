06c41dc1091ef761e2f3e80398e41e724a3cba93
Yu Ishiguro <yuuis.egr@gmail.com>
2019-08-17 19:12:00 +0900

    add routes and server

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,3 +1,32 @@
-object Server {
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+import dao.CatDao
+import model.Cat
+import http.CatRoutes
 
+import scala.io.StdIn
+import scala.concurrent.ExecutionContextExecutor
+
+object Server extends App with CatRoutes with CatDao {
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val materializer = ActorMaterializer()
+  implicit val dispatcher: ExecutionContextExecutor = system.dispatcher
+
+  ddl.onComplete { _ =>
+    // seeding
+    create(Cat("Bailey"))
+    create(Cat("Max"))
+    create(Cat("Charlie"))
+    create(Cat("Bella"))
+    create(Cat("Lucy"))
+    create(Cat("Molly"))
+
+    val bindingFuture = Http().bindAndHandle(routes, "localhost", 8080)
+
+    println(s"server online at http://localhost:8080/\npress return to stop.")
+
+    StdIn.readLine()
+    bindingFuture.flatMap(_.unbind()).onComplete(_ => system.terminate())
+  }
 }
--- src/main/scala/http/CatRoutes.scala
+++ src/main/scala/http/CatRoutes.scala
@@ -1,5 +1,45 @@
 package http
 
-class CatRoutes {
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.model.HttpResponse
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.directives.MethodDirectives
+import dao.CatDao
+import model.Cat
 
+import scala.concurrent.ExecutionContextExecutor
+
+trait CatRoutes extends SprayJsonSupport {
+  this: CatDao =>
+  implicit val dispatcher: ExecutionContextExecutor
+
+  val routes = pathPrefix("cats") {
+    pathEnd {
+      get {
+        complete(getAll)
+      } ~ post {
+        entity(as[Cat]) { cat =>
+          complete {
+            create(cat).map{ result => HttpResponse(entity = "dog has been saved successfully.")}
+          }
+        }
+      }
+    } ~
+      path(IntNumber) { id =>
+        get {
+          complete(getById(id))
+        } ~ put {
+          entity(as[Cat]) { cat =>
+            complete{
+              val newCat = Cat(cat.name, Option(id))
+              update(newCat).map{ result => HttpResponse(entity = "dog has been updated successfully.")}
+            }
+          }
+        } ~ MethodDirectives.delete {
+          complete {
+            delete(id).map{ result => HttpResponse(entity = "dog has been deleted successfully.")}
+          }
+        }
+      }
+  }
 }
